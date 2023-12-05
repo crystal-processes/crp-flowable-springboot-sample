a2e73532fcb7652aeaae4f717f2ce0c4ce963b43
Martin Grofčík <mgrofcik@gmail.com>
2023-12-05 19:48:49 +0100

    #1 rest api tests

--- src/test/java/org/crp/flowable/springboot/sample/CrpFlowableSpringBootSampleApplicationTests.java
+++ src/test/java/org/crp/flowable/springboot/sample/CrpFlowableSpringBootSampleApplicationTests.java
@@ -1,13 +1,20 @@
 package org.crp.flowable.springboot.sample;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CrpFlowableSpringBootSampleApplicationTests {
 
+	@Autowired
+	private CrpFlowableSpringBootSampleApplication application;
+
 	@Test
 	void contextLoads() {
+		assertThat(application).isNotNull();
 	}
 
 }
