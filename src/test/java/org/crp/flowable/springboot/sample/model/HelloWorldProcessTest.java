89fbdf46cea2e2235e8e34dc86ffe960d2584667
Martin Grofčík <mgrofcik@gmail.com>
2023-12-06 15:28:24 +0100

    #3 fluent assertions implementation

--- src/test/java/org/crp/flowable/springboot/sample/model/HelloWorldProcessTest.java
+++ src/test/java/org/crp/flowable/springboot/sample/model/HelloWorldProcessTest.java
@@ -16,50 +16,44 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.crp.flowable.assertions.CrpFlowableAssertions.assertThat;
 
 @AcmeApplicationTest
 public class HelloWorldProcessTest {
 
     @Test
-    void followHappyPathWithFluentAssertions(RuntimeService runtimeService, TaskService taskService, HistoryService historyService) {
+    void followHappyPathWithFluentAssertions(RuntimeService runtimeService, TaskService taskService) {
         ProcessInstance helloWorldProcess = runtimeService.createProcessInstanceBuilder().processDefinitionKey("P001-helloWorld").start();
 
-        assertThat(helloWorldProcess).isNotNull();
         List<String> path = new ArrayList<>(List.of("theStart", "theStart-theSayHelloUserTask", "theSayHelloUserTask"));
-        assertThat(runtimeService.createActivityInstanceQuery()
-                .processInstanceId(helloWorldProcess.getId()).orderByActivityInstanceStartTime().asc().list())
-                .as("The hello world process has to go directly through theStart -> theSayHelloUserTask")
+        assertThat(helloWorldProcess)
+                .isRunning()
+                .hasVariableWithValue("initiator", null)
+                .activities()
                 .extracting(ActivityInstance::getActivityId)
                 .containsExactlyInAnyOrderElementsOf(path);
-        assertThat(runtimeService.createVariableInstanceQuery().processInstanceId(helloWorldProcess.getId()).list())
-                .extracting("name", "value")
-                .containsExactly(Tuple.tuple("initiator", null));
+        assertThat(helloWorldProcess)
+                .userTasks()
+                .extracting("name", "taskDefinitionKey")
+                .containsExactly(Tuple.tuple("Say hello world", "theSayHelloUserTask"));
 
         Task userTask = taskService.createTaskQuery().processInstanceId(helloWorldProcess.getId()).singleResult();
-
-        assertThat(userTask).extracting(Task::getName).isEqualTo("Say hello world");
-        assertThat(userTask).extracting(Task::getTaskDefinitionKey).isEqualTo("theSayHelloUserTask");
-
         taskService.complete(userTask.getId(), Map.of("greeting", "Hello World!"));
 
-        assertThat(runtimeService.createActivityInstanceQuery().processInstanceId(helloWorldProcess.getId()).singleResult())
+        assertThat(helloWorldProcess)
                 .as("The hello world process is finished, no instance is present in the runtime.")
-                .isNull();
-        HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery().processInstanceId(helloWorldProcess.getId()).singleResult();
-        assertThat(historicProcessInstance)
-                .as("The hello world process must be present in the history")
-                .isNotNull();
-        assertThat(historicProcessInstance)
-                .as("The hello world process must be finished")
-                .extracting(HistoricProcessInstance::getEndTime).isNotNull();
-        assertThat(historyService.createHistoricVariableInstanceQuery().processInstanceId(helloWorldProcess.getId()).list())
+                .doesNotExist()
+                .inHistory()
+                .as("The hello world process must be present in the history and finished")
+                .isFinished()
+                .variables()
                 .as("All variables must be present in the history")
                 .extracting("name", "value")
                 .containsExactlyInAnyOrder(Tuple.tuple("initiator", null), Tuple.tuple("greeting", "Hello World!"));
         path.addAll(List.of("theSayHelloUserTask-theEnd", "theEnd"));
-        assertThat(historyService.createHistoricActivityInstanceQuery().processInstanceId(helloWorldProcess.getId()).orderByHistoricActivityInstanceStartTime().asc().list())
+        assertThat(helloWorldProcess).inHistory()
                 .as("The hello world process must pass through all activities")
+                .activities()
                 .extracting(HistoricActivityInstance::getActivityId)
                 .containsExactlyInAnyOrderElementsOf(path);
     }
@@ -93,9 +87,9 @@ public class HelloWorldProcessTest {
         assertThat(historicProcessInstance)
                 .as("The hello world process must be present in the history")
                 .isNotNull();
-        assertThat(historicProcessInstance)
+        assertThat(historicProcessInstance.getEndTime())
                 .as("The hello world process must be finished")
-                .extracting(HistoricProcessInstance::getEndTime).isNotNull();
+                .isNotNull();
         assertThat(historyService.createHistoricVariableInstanceQuery().processInstanceId(helloWorldProcess.getId()).list())
                 .as("All variables must be present in the history")
                 .extracting("name", "value")
