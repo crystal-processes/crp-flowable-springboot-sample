a7791d094dc767b4a237ecc727b5bba4191a2f7d
Martin Grofčík <mgrofcik@gmail.com>
2023-12-17 20:22:07 +0100

    #8 use h2 for tests

--- src/test/java/org/crp/flowable/springboot/sample/AcmeApplicationContextTest.java
+++ src/test/java/org/crp/flowable/springboot/sample/AcmeApplicationContextTest.java
@@ -3,10 +3,12 @@ package org.crp.flowable.springboot.sample;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AcmeApplicationContextTest {
 
 	@Autowired
